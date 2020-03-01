5ab0abb4727879277631efab8d1f165d6866d50d
YuChih Hu <hijac.hu@gmail.com>
2020-03-01 16:51:39 +0800

    interaction testing

--- src/app/06-services/todos.component.spec.ts
+++ src/app/06-services/todos.component.spec.ts
@@ -1,6 +1,6 @@
 import { TodosComponent } from './todos.component';
 import { TodoService } from './todo.service';
-import { from } from 'rxjs';
+import { from, empty, throwError } from 'rxjs';
 
 
 describe('TodosComponent', () => {
@@ -22,5 +22,34 @@ describe('TodosComponent', () => {
     component.ngOnInit();
 
     expect(component.todos).toBe(todos);
+  });
+
+  it('should call the server to save the changes when a new todo item is added', () => {
+    const spy = spyOn(service, 'add').and.callFake((t) => {
+      // return empty();
+      return from([]);
+    });
+
+    component.add();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should add the new todo returned from the server', () => {
+    const todo = { id: 1 };
+    const spy = spyOn(service, 'add').and.returnValue(from([ todo ]));
+
+    component.add();
+
+    expect(component.todos.indexOf(todo)).toBeGreaterThan(-1);
+  });
+
+  it('should set the message property if the server returns an error when adding a new todo', () => {
+    const error = 'error from the server';
+    const spy = spyOn(service, 'add').and.returnValue(throwError(error));
+
+    component.add();
+
+    expect(component.message).toBe(error);
   });
 });
